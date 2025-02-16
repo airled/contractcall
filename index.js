2d7fd16c9588d640855d7b63a2493129fd15b58c
Uladzimir <nilstruct@gmail.com>
2025-02-16 09:09:26 +0600

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const { ethers } = require("ethers");
 const rpcUrl = ''
 const contractAddress = ''
 const abi = []
+const privateKey = ''
 
 async function main() {
   const provider = new ethers.JsonRpcProvider(rpcUrl);
-  const signer = new ethers.Wallet("", provider)
+  const signer = new ethers.Wallet(privateKey, provider)
   const contract = new ethers.Contract(contractAddress, abi, signer);
   // contract.name().then(console.log)
 }
