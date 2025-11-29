2e7cf2b8a9d6b2fef82a6a635035862d71ae18f7
nilstruct <nilstruct@gmail.com>
2025-11-29 10:50:26 +0600

    Use env file

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+require('dotenv').config();
+
 const { ethers } = require("ethers");
 
 const rpcUrl = ''
 const contractAddress = ''
 const abi = []
-const privateKey = ''
 
 async function main() {
   const provider = new ethers.JsonRpcProvider(rpcUrl);
-  const signer = new ethers.Wallet(privateKey, provider)
+  const signer = new ethers.Wallet(process.env.KEY, provider)
   const contract = new ethers.Contract(contractAddress, abi, signer);
   // contract.name().then(console.log)
 }
